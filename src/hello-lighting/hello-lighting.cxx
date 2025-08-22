e52fcd2f784dbb9b4e218472909fc5018b5c9877
Vladislav Riabov <vladislav.riabov@outlook.com>
2025-08-22 17:46:32 +0100

    Fix texture path

--- src/hello-lighting/hello-lighting.cxx
+++ src/hello-lighting/hello-lighting.cxx
@@ -55,7 +55,7 @@ void HelloLighting::LoadAssets()
   glGenTextures(1, &texture);
   glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_2D, texture);
-  ProcessImage(GetCurrentExecutableDirectory() / "assets/textures/LearnOpenGL/wooden_container.jpg", [](stb::Image& image)
+  ProcessImage(GetCurrentExecutableDirectory() / "assets/textures/LearnOpenGL/container.jpg", [](stb::Image& image)
     {
       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.Width(), image.Height(), 0, GL_RGB, GL_UNSIGNED_BYTE, image.Bytes());
     });
